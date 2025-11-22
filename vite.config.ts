a30c53983729cd3dd44135dbc3249da71d80cbb1
UC Ideiak <ucideiak@proton.me>
2025-11-22 21:40:48 +0100

    Cambios (vite.config.ts) para que el build muestre MULTIPAGE.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        login: './login.html',
+        contacto: './contacto.html',
+        thanks: './thanks.html',
+        no_login: './no_login.html',
+      }
+    }
+  }
 })
